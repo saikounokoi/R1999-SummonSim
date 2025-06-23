ea27ed6475ca056798dfa115dbb89000a8c88468
saikounokoi <neji25935@gmail.com>
2025-06-23 21:15:32 +0700

    Update DisplayResult.js

--- scripts/DisplayResult.js
+++ scripts/DisplayResult.js
@@ -1,5 +1,8 @@
 let ClearDrop = 0;
-let TotalPulls = 0
+let TotalPulls = 0;
+function SlotRender(id, rarity, afflatus, name, prydwen) {
+	document.getElementById('result').innerHTML += "<a class='CharacterSlot' target='_blank' href='https://www.prydwen.gg/re1999/characters/" + prydwen + "'><div style='position: relative'><img src='../../assets/" + id + ".png' style='width: 100%'><img src='../../img/" + rarity + ".png' style='position: absolute; left: 0%; bottom: 0%; width: 100%'><img src='../../img/" + afflatus + ".png' style='position: absolute; left: 0%; top: 0%; width: 20%'><p style='color: white; font-size: 70%; position: absolute; bottom: -5%; left: 50%; transform: translate(-50%, -50%);'>" + name + "</p></div></a>"
+};
 function DisplayResult(x) {
 	document.getElementById('banner').style.display = 'none';
 	document.getElementById('result').textContent = '';
